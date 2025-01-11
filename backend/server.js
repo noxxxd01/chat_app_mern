59cfb9d1bda4db9b8c4ffb0580260108f87e1dd4
mikkozz <mikko@arkdesign.nl>
2025-01-11 08:55:37 +0800

    update

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,9 @@ import userRouter from "./routes/user.route.js";
 import connectDb from "./config/connectDb.js";
 import { app, server } from "./socket/socket.js";
 import express from "express";
+import path from "path";
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -15,6 +18,11 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRouter);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
